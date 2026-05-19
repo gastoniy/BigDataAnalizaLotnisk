8aced365cfb04584590415eed614518dda6ea4d2
gastoniy <279533@student.pwr.edu.pl>
2026-05-19 11:35:55 +0200

    Update database; Add note about 19.05 meeting

--- training.py
+++ training.py
@@ -3,11 +3,11 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
-from data_transform import FlightsTransform # transformation module 
+from data_transform_alt import FlightsTransform # transformation module 
 
 # initial transform of the raw data block 
-transformer = FlightsTransform("dataset_loty_krakow_20260513_183527.csv")
-sanitized_df = transformer.transform(threshold_minutes=15) # since the model itself is going to be saved, there's no point in savng csv now
+transformer = FlightsTransform("dataset_loty_krakow_20260518_183551.csv")
+sanitized_df = transformer.transform(threshold_minutes=15, encoding="label") # since the model itself is going to be saved, there's no point in savng csv now
 
 # dropping the unwanted stuff to prevent data leakage
 X = sanitized_df.drop(['czy_opozniony', 'czas_rzeczywisty_unix'], axis=1)
@@ -30,5 +30,5 @@ final_model.fit(X, y)
 # since we need to preserve trained model's state 
 # and hot encoder, to not confuse model later on
 # we are saving their state using joblib module 
-joblib.dump(final_model, 'flights_random_forest.joblib')
-joblib.dump(transformer.encoder, "airline_hot_encoder.joblib")
+joblib.dump(final_model, 'flights_random_forest_label.joblib')
+joblib.dump(transformer.encoder, "airline_label.joblib")
